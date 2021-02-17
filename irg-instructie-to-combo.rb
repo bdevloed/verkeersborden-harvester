07e7a1e18f306eb0532e1d728be37e7aab5222cf
Boris De Vloed <boris.de.vloed@gmail.com>
2021-02-17 16:49:05 +0100

    parse measure combinations

--- irg-instructie-to-combo.rb
+++ irg-instructie-to-combo.rb
@@ -9,48 +9,37 @@ class InstructieHarvester
   DC = RDF::Vocab::DC
   PROV = RDF::Vocab::PROV
   RDFS = RDF::Vocab::RDFS
-  REGORG = RDF::Vocabulary.new("https://www.w3.org/ns/regorg#")
-  MU = RDF::Vocabulary.new("http://mu.semte.ch/vocabularies/core/")
-  NFO = RDF::Vocabulary.new("http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#")
-  NIE =  RDF::Vocabulary.new("http://www.semanticdesktop.org/ontologies/2007/01/19/nie#")
-  DBPEDIA = RDF::Vocabulary.new("http://dbpedia.org/ontology/")
+  REGORG = RDF::Vocabulary.new('https://www.w3.org/ns/regorg#')
+  MU = RDF::Vocabulary.new('http://mu.semte.ch/vocabularies/core/')
+  NFO = RDF::Vocabulary.new('http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#')
+  NIE = RDF::Vocabulary.new('http://www.semanticdesktop.org/ontologies/2007/01/19/nie#')
+  DBPEDIA = RDF::Vocabulary.new('http://dbpedia.org/ontology/')
   ADMS = RDF::Vocabulary.new('http://www.w3.org/ns/adms#')
   SV = RDF::Vocabulary.new('http://www.w3.org/2003/06/sw-vocab-status/ns#')
 
-  MOB = RDF::Vocabulary.new("https://data.vlaanderen.be/ns/mobiliteit#")
-  MANDAAT = RDF::Vocabulary.new("http://data.vlaanderen.be/ns/mandaat#")
-  BESLUIT = RDF::Vocabulary.new("http://data.vlaanderen.be/ns/besluit#")
-  EXT = RDF::Vocabulary.new("http://mu.semte.ch/vocabularies/ext/")
-  LBLOD_MOW = RDF::Vocabulary.new("http://data.lblod.info/vocabularies/mobiliteit/")
+  MOB = RDF::Vocabulary.new('https://data.vlaanderen.be/ns/mobiliteit#')
+  MANDAAT = RDF::Vocabulary.new('http://data.vlaanderen.be/ns/mandaat#')
+  BESLUIT = RDF::Vocabulary.new('http://data.vlaanderen.be/ns/besluit#')
+  EXT = RDF::Vocabulary.new('http://mu.semte.ch/vocabularies/ext/')
+  LBLOD_MOW = RDF::Vocabulary.new('http://data.lblod.info/vocabularies/mobiliteit/')
 
   def initialize(input_verkeersborden, input_instructie)
     @repo = RDF::Graph.load(input_verkeersborden)
     @csv_path = input_instructie
-    @output = "output/verkeersborden-combinaties.ttl"
+    @output = 'output/verkeersborden-combinaties.ttl'
   end
-
 
   def harvest
     index = 1
     begin
       RDF::Graph.new do |graph|
-        ::CSV.foreach(@csv_path, {  headers: :first_row, encoding: 'utf-8', quote_char: "\""  }) do |row|
-          statements = parse_row(index, row)
-          if statements.length > 0
+        data = CSV.read(@csv_path, { headers: :first_row, encoding: 'utf-8', quote_char: '"' })
+        data['maatregel_naam'].uniq.each do |measure_name|
+          statements = road_sign_combinations(measure_name)
+          if statements.length.positive?
             graph.insert_statements(statements)
           end
           index += 1
-        end
-        query = %(
-SELECT ?combinatie (GROUP_CONCAT(?part) as ?parts)
-WHERE {
-      ?combinatie a <#{LBLOD_MOW.Verkeersbordcombinatie}>; <#{DC.hasPart}> ?part.
-} GROUP BY ?combinatie HAVING (COUNT(?part) < 2)
-)
-        SPARQL.execute(query, graph).each_solution do |solution|
-          puts "combinatie #{solution[:combinatie]} heeft maar 1 bord en wordt verwijderd"
-          graph.delete([solution[:combinatie], nil, nil])
-          graph.delete([RDF::URI(solution[:parts]), nil, nil])
         end
         File.write(@output, graph.dump(:ttl), mode: 'w')
       end
@@ -61,71 +50,71 @@ WHERE {
   end
 
   def find_verkeersbord(verkeersbord_code)
-    if verkeersbord_code.nil?
-      return nil
-    end
-    if index = verkeersbord_code.index("Type")
-      extract = verkeersbord_code.slice(index+5,verkeersbord_code.length)
+    return nil if verkeersbord_code.nil?
+
+    if index = verkeersbord_code.index('Type')
+      extract = verkeersbord_code.slice(index + 5, verkeersbord_code.length)
       puts "mapped #{verkeersbord_code} to G#{extract}"
-      verkeersbord_code= "G#{extract}"
+      verkeersbord_code = "G#{extract}"
+    end
+
+    if verkeersbord_code.match /^X[a-z]$/
+      mapped = "G#{verkeersbord_code}"
+      puts "mapped #{verkeersbord_code} to #{mapped}"
+      verkeersbord_code = mapped
     end
     query = RDF::Query.new({
                              bord: {
-                               RDF.type  => MOB['Verkeersbordconcept'],
+                               RDF.type => MOB['Verkeersbordconcept'],
                                SKOS.prefLabel => verkeersbord_code
                              }
                            })
     result = query.execute(@repo)
-    if result.length === 1
-      return result.first[:bord]
-    else
-      return nil
-    end
-
+    result.first[:bord] if result.length === 1
   end
-  def parse_row(index, row)
-    uuid = row.first[1]
-    if uuid
-      row_iri = RDF::URI("http://data.lblod.info/measure-concept/#{uuid}")
-      verkeersbord_code = row["verkeersbord_code"]
-      verkeersbord_iri = find_verkeersbord(verkeersbord_code)
-      verkeersbord_instructie = row["instructie"]
-      verkeersbord_aanvullende_instructie = row["aanvullende_instructie"]
-      measure_name = row["maatregel_naam"]
-      instruction_names = measure_name.to_s.split('/', -1)
-      subInstructions = []
-      instructions.each do |instruction|
-        subInstructions << instruction.split(/[-+]/, -1)
-      end
-      # TODO - use parsed subInstructions
-      statements = []
-      if verkeersbord_iri
-        instruction_uuid = Digest::MD5.hexdigest("#{uuid}#{verkeersbord_iri}")
-        instruction_concept = RDF::URI("http://data.lblod.info/instruction-concept/#{instruction_uuid}")
-        statements << RDF::Statement.new( row_iri, RDF.type, LBLOD_MOW["MeasureConcept"])
-        statements << RDF::Statement.new( row_iri, LBLOD_MOW['hasInstruction'], instruction_concept )
-        statements << RDF::Statement.new( row_iri, MU.uuid, RDF::Literal.new(uuid))
-        statements << RDF::Statement.new( instruction_concept, RDF.type, LBLOD_MOW["Instruction"])
-        statements << RDF::Statement.new( instruction_concept, MU.uuid, instruction_uuid)
-        statements << RDF::Statement.new( instruction_concept, DC.description, RDF::Literal.new(verkeersbord_instructie))
-        statements << RDF::Statement.new( instruction_concept, LBLOD_MOW['verkeersbordconcept'], verkeersbord_iri)
 
-        sub_instruction_uuid = Digest::MD5.hexdigest("#{verkeersbord_aanvullende_instructie}")
-        sub_instruction_concept = RDF::URI("http://data.lblod.info/instruction-concept/#{sub_instruction_uuid}")
-        statements << RDF::Statement.new( instruction_concept, LBLOD_MOW['hasSubInstruction'], sub_instruction_concept )
-        statements << RDF::Statement.new( sub_instruction_concept, RDF.type, LBLOD_MOW["Instruction"])
-        statements << RDF::Statement.new( sub_instruction_concept, MU.uuid, sub_instruction_uuid)
-        statements << RDF::Statement.new( sub_instruction_concept, DC.description, RDF::Literal.new(verkeersbord_aanvullende_instructie))
-        # statements << RDF::Statement.new( sub_instruction_concept, LBLOD_MOW['verkeersbordconcept'], verkeersbord_iri)
-
-      else
-        puts "row #{index} no road sign found for code #{verkeersbord_code.inspect}"
-      end
-      statements
-    else
-      puts "row #{index} has no uuid"
-      []
+  def parse_measure(measure_name)
+    combinations = measure_name.to_s.split('/', -1)
+    combinations_with_subsigns = []
+    combinations.each do |combination|
+      combinations_with_subsigns << combination.split(/[-+]/, -1)
     end
+    combinations_with_subsigns
+  end
+
+  def road_sign_combinations(measure_name)
+    statements = []
+    combinations_with_subsigns = parse_measure(measure_name)
+    if combinations_with_subsigns.length.positive?
+      main_sign_combinations = []
+      combinations_with_subsigns.each do |combination|
+        if combination.length.positive?
+          main_sign_iri = find_verkeersbord(combination[0])
+          if main_sign_iri
+            main_sign_combinations << main_sign_iri
+            subsigns = combination[1..-1]
+            subsigns.each do |subsign|
+              sub_sign_iri = find_verkeersbord(subsign)
+              if sub_sign_iri
+                statements << RDF::Statement.new(main_sign_iri, LBLOD_MOW['heeftOnderbordConcept'], sub_sign_iri)
+              else
+                puts "no road sign found for subsign with code #{subsign}"
+              end
+            end
+          else
+            puts "no road sign found for code #{combination[0]}"
+          end
+        else
+          puts "#{combination} has no main sign"
+        end
+      end
+      main_sign_combinations.permutation(2).to_a.each do |main_sign_combination|
+        statements << RDF::Statement.new(main_sign_combination[0], LBLOD_MOW['heeftGerelateerdVerkeersbordconcept'], main_sign_combination[1])
+      end
+    else
+      puts "#{measure_name} has no road sign combination"
+    end
+    statements
   end
 end
 
